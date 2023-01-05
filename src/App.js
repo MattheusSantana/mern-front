ea4d0fb0883e726459b7f3d69427ef9f3a998ffe
MattheusSantana <matheusess@hotmail.com>
2023-01-04 20:56:12 -0400

    adding global styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import GlobalStyle from './styles/global.js';
+
 
 const App = () => {
   return (
+    <>
     <div>App</div>
+    <GlobalStyle />
+    </>
   )
 }
 
